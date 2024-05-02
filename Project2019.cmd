7bbe7a248e751ea0677088d28a79e979b32de87c
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-02 18:17:12 +0700

    Add files via upload

---
+++ Project2019.cmd
@@ -0,0 +1,28 @@
+@echo off
+title Activate Microsoft Project 2019 (ALL versions) for FREE - https://github.com/BsNgChiThanh 
+
+
+echo - Activating Microsoft Project Professional Plus 2019...
+
+
+cd /d %ProgramFiles%\Microsoft Office\Office16 
+cd /d %ProgramFiles(x86)%\Microsoft Office\Office16 
+
+
+cscript //nologo ospp.vbs /inslic:"..\root\Licenses16\pkeyconfig-office.xrm-ms" >nul
+(for /f %%x in ('dir /b ..\root\Licenses16\client-issuance*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
+(for /f %%x in ('dir /b ..\root\Licenses16\projectprovl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
+
+
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /inpkey:B4NPR-3FKK7-T2MBV-FRQ4W-PKD2B
+cscript ospp.vbs /inpkey:C4F7P-NCP8C-6CQPT-MQHV9-JXD2M
+cscript ospp.vbs /sethst:107.175.77.7
+cscript ospp.vbs /act
+
+
+
+:end
+:notsupported
+:halt
+pause >nul
