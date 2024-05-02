0ad516b62b2856428d53b8b6dc3690dc3e2a9dde
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-03 06:36:46 +0700

    Add files via upload

---
+++ ActiveOffice2021.cmd
@@ -0,0 +1,24 @@
+@echo off
+title Activate Microsoft Office 2021 (ALL versions) for FREE - https://github.com/BsNgChiThanh
+cls
+ 
+
+(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")
+(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")
+
+(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
+
+ 
+echo Activating your Microsoft Office 2021...
+
+
+script ospp.vbs /setprt:1688
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+cscript ospp.vbs /sethst:107.175.77.7
+cscript ospp.vbs /act
+
+
+:end
+:notsupported
+:halt
+pause >nul
