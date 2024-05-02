86e738972ee8c0a4af4ab760461315c8764b49d3
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-02 17:07:59 +0700

    Add files via upload

---
+++ Visio2019.cmd
@@ -0,0 +1,28 @@
+@echo off
+title Activate Microsoft Visio 2019 (ALL versions) for FREE - https://github.com/BsNgChiThanh 
+cls
+
+echo Activating Microsoft Visio 2019...
+ 
+
+(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")
+(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")
+
+
+cscript ospp.vbs /inslic:"..\root\Licenses16\pkeyconfig-office.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\client-issuance*.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\visioprovl_kms*.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\visiopro2019vl_kms*.xrm-ms"
+
+
+cscript ospp.vbs /inpkey:9BGNQ-K37YR-RQHF2-38RQ3-7VCBB
+cscript ospp.vbs /sethst:107.175.77.7
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /act
+
+
+:end
+:notsupported
+:halt
+pause >nul
+
