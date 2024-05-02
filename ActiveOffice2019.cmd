9ca473e484929e87e03acf3c23ad10eb9dddfc27
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-02 11:29:26 +0700

    Update ActiveOffice2019.cmd

--- ActiveOffice2019.cmd
+++ ActiveOffice2019.cmd
@@ -15,7 +15,6 @@ echo.
 
 cscript //nologo ospp.vbs /unpkey:6MWKP >nul
 cscript //nologo ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP >nul
-cscript //nologo ospp.vbs /inpkey:6NWWJ-YQWMR-QKGCB-6TMB3-9D9HK >nul
 cscript ospp.vbs /sethst:107.175.77.7
 cscript ospp.vbs /setprt:1688
 cscript ospp.vbs /act
@@ -23,11 +22,13 @@ cscript ospp.vbs /act
 
 set i=1
 :server
-if %i%==1 set KMS_Sev=kms7.MSGuides.com
-if %i%==2 set KMS_Sev=s8.uk.to
-if %i%==3 set KMS_Sev=s9.uk.to
+if %i%==1 set setprt:1688
+if %i%==2 set sethst:107.175.77.7
+if %i%==3 set KMS_Sev=e9.us.to
 if %i%==4 goto notsupported
-cscript //nologo ospp.vbs /sethst:%KMS_Sev% >nul
+cscript //nologo ospp.vbs /sethst:%KMS% >nul
+
+
 
 
 :end
