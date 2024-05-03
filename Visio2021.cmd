9292306a9c9768efc2d15f88f42d356cf6e88b76
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-03 17:40:50 +0700

    Update Visio2021.cmd

--- Visio2021.cmd
+++ Visio2021.cmd
@@ -6,10 +6,13 @@ echo Activating Microsoft Visio 2021...
 
 cd /d %ProgramFiles%\Microsoft Office\Office16
 cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
+
 cscript ospp.vbs /inslic:"..\root\Licenses16\pkeyconfig-office.xrm-ms"
 (for /f %x in ('dir /b ..\root\Licenses16\client-issuance*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x")
 (for /f %x in ('dir /b ..\root\Licenses16\visioprovl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x")
 (for /f %x in ('dir /b ..\root\Licenses16\visiopro2021vl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x")
+
+echo. 
 
 cscript ospp.vbs /setprt:1688
 cscript ospp.vbs /inpkey:KNH8D-FGHT4-T8RK3-CTDYJ-K2HT4
@@ -19,7 +22,7 @@ cscript ospp.vbs /sethst:172.16.0.2
 cscript ospp.vbs /act
 
 
-
+timeout 10
 :end
 :notsupported
 :halt
