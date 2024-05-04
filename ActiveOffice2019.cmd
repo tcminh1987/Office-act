de7205ba8e5d7ca88da1048397c8c0c4b0640173
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-04 14:20:08 +0700

    Update ActiveOffice2019.cmd

--- ActiveOffice2019.cmd
+++ ActiveOffice2019.cmd
@@ -1,6 +1,8 @@
 @echo off
 title Kich hoat Microsoft Office 2019 ALL versions - https://github.com/BsNgChiThanh
 cls
+color F0
+mode con cols=98 lines=30
 
 echo Activating your Microsoft Office 2019...
  
@@ -18,6 +20,10 @@ cscript ospp.vbs /sethst:107.175.77.7
 cscript ospp.vbs /sethst:172.16.0.2
 cscript ospp.vbs /act
 
+
+echo.
+echo.
+echo Nhan phim bat ky de thoat.
 
 
 :end
