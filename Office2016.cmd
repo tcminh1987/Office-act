35e621df99eb66f8844ba7a09a0cff114ba18e7c
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-04 14:21:01 +0700

    Update Office2016.cmd

--- Office2016.cmd
+++ Office2016.cmd
@@ -1,6 +1,9 @@
 @echo off
 title  Activate Microsoft Office Prolus 2016 for FREE - https://github.com/BsNgChiThanh 
 cls
+color F0
+mode con cols=98 lines=30
+
 echo Activating your Microsoft Office prolus 2016...
  
 (if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")
@@ -17,6 +20,10 @@ cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
 cscript ospp.vbs /sethst:107.175.77.7
 cscript ospp.vbs /sethst:172.16.0.2
 cscript ospp.vbs /act
+
+echo.
+echo.
+echo Nhan phim bat ky de thoat.
 
 
 :end
