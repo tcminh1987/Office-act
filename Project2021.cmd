23dfea2b0ce11c3df8fbe05dc45903534cf53cbb
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-04 07:51:45 +0700

    Add files via upload

---
+++ Project2021.cmd
@@ -0,0 +1,29 @@
+
+@echo off
+title Activate Microsoft Project 2021 for FREE - https://github.com/BsNgChiThanh 
+cls
+echo Activating Microsoft Project 2021...
+
+
+
+
+cd /d %ProgramFiles%\Microsoft Office\Office16
+cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
+cscript ospp.vbs /inslic:"..\root\Licenses16\pkeyconfig-office.xrm-ms"
+(for /f %x in ('dir /b ..\root\Licenses16\client-issuance*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x")
+(for /f %x in ('dir /b ..\root\Licenses16\projectprovl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x")
+(for /f %x in ('dir /b ..\root\Licenses16\projectpro2021vl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x")   
+     
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /inpkey:FTNWT-C6WBT-8HMGF-K9PRX-QV9H8
+cscript ospp.vbs /inpkey:J2JDC-NJCYY-9RGQ4-YXWMH-T3D4T
+cscript ospp.vbs /sethst:107.175.77.7
+cscript ospp.vbs /sethst:172.16.0.2
+cscript ospp.vbs /act
+
+
+
+:end
+:notsupported
+:halt
+pause >nul
