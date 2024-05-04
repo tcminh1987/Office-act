f21d6f211d79c197a4d46824ed8aff8c4cd87225
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-04 14:19:18 +0700

    Update ActiveOffice2021.cmd

--- ActiveOffice2021.cmd
+++ ActiveOffice2021.cmd
@@ -1,6 +1,8 @@
 @echo off
 title Activate Microsoft Office 2021 (ALL versions) for FREE - https://github.com/BsNgChiThanh
 cls
+color F0
+mode con cols=98 lines=30
 
 echo Activating your Microsoft Office 2021...
 
@@ -16,6 +18,11 @@ cscript ospp.vbs /inpkey:KDX7X-BNVR8-TXXGX-4Q7Y8-78VT3
 cscript ospp.vbs /sethst:107.175.77.7
 cscript ospp.vbs /sethst:172.16.0.2
 cscript ospp.vbs /act
+
+
+echo.
+echo.
+echo Nhan phim bat ky de thoat.
 
 
 :end
