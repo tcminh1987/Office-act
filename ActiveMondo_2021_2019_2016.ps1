f0b5f892ca8ef1beb835ce330fae0fbdd948ba44
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-09 22:22:57 +0700

    Update ActiveMondo_2021_2019_2016.ps1

--- ActiveMondo_2021_2019_2016.ps1
+++ ActiveMondo_2021_2019_2016.ps1
@@ -1,10 +1,10 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/BsNgChiThanh/Kich-hoat-Office/KichHoatOffice/ActiveMondo_2021_2019_2016.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/BsNgChiThanh/Kich-hoat-Office/KichHoatOffice/Mondo%2C2021%2C2019%2C2016.cmd'
 
 $rand = Get-Random -Maximum 1000
-$FilePath = "$env:TEMP\ActiveMondo_2021_2019_2016_$rand.cmd"
+$FilePath = "$env:TEMP\Mondo%2C2021%2C2019%2C2016_$rand.cmd"
 
 try {
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
