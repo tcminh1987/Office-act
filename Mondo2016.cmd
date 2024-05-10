cd747a139d40a6dec7f0bca698fa81787ab40b23
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-10 10:31:39 +0700

    Update Mondo2016.cmd

--- Mondo2016.cmd
+++ Mondo2016.cmd
@@ -39,7 +39,7 @@ if errorlevel 1 goto:365Mondo
 cls
 
 color F0
-mode con cols=200 lines=200
+mode con cols=150 lines=150
 echo Xem trang thai Office cua ban...
 
 (if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")
