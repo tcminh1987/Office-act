971b0861e37fd473fd0e0329c9007ff68f168c4b
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-05-14 12:09:40 +0700

    Update Mondo,2021,2019,2016.cmd

--- Mondo,2021,2019,2016.cmd
+++ Mondo,2021,2019,2016.cmd
@@ -580,6 +580,7 @@ if exist "%ProgramFiles(x86)%\Microsoft Office\Office%v%\ospp.vbs" cd /d "%Progr
 cscript ospp.vbs /setprt:1688
 cscript OSPP.VBS /inpkey:C2FG9-N6J68-H8BTJ-BW3QX-RM3B3
 cscript OSPP.VBS /inpkey:J484Y-4NKBF-W2HMG-DBMJC-PGWR7
+cscript OSPP.VBS /inpkey:RNQR6-67XM2-Y9V4R-7HQQH-94QB4
 cscript ospp.vbs /sethst:107.175.77.7
 cscript ospp.vbs /sethst:172.16.0.2
 cscript ospp.vbs /act
