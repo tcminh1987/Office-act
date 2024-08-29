d8fa93fa3a6f923d7173a686fca6c350e339965a
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-08-29 15:18:16 +0700

    Update Office2016.cmd

--- Office2016.cmd
+++ Office2016.cmd
@@ -17,6 +17,7 @@ echo Activating your Microsoft Office prolus 2016...
 cscript ospp.vbs /setprt:1688
 cscript ospp.vbs /inpkey:JNRGM-WHDWX-FJJG3-K47QV-DRTFM
 cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
+cscript ospp.vbs /inpkey:T4NR6-38X6Q-RFQTM-XPRBT-XQBVM
 cscript ospp.vbs /sethst:107.175.77.7
 cscript ospp.vbs /sethst:172.16.0.2
 cscript ospp.vbs /act
