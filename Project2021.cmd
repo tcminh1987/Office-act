696775655de83d2550443b086d685711025a9b88
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-10-22 09:35:55 +0700

    Update Project2021.cmd

--- Project2021.cmd
+++ Project2021.cmd
@@ -1,17 +1,19 @@
-
 @echo off
-title Activate Microsoft Project 2021 for FREE - https://github.com/BsNgChiThanh 
+title Activate Microsoft Project 2021 (ALL versions) for FREE - https://github.com/BsNgChiThanh 
 cls
-
 color F0
 mode con cols=98 lines=30
 
-echo Activating Microsoft Project 2021...
+echo Activating Microsoft Project Professional Plus 2021...
 
 
-cd /d %ProgramFiles%\Microsoft Office\Office16
-cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
+(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")
+(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")
 
+
+cscript //nologo ospp.vbs /inslic:"..\root\Licenses16\pkeyconfig-office.xrm-ms" >nul
+(for /f %%x in ('dir /b ..\root\Licenses16\client-issuance*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
+(for /f %%x in ('dir /b ..\root\Licenses16\projectprovl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
 
 
 cscript ospp.vbs /setprt:1688
@@ -21,9 +23,11 @@ cscript ospp.vbs /sethst:107.175.77.7
 cscript ospp.vbs /sethst:172.16.0.2
 cscript ospp.vbs /act
 
+
 echo.
 echo.
 echo Nhan phim bat ky de thoat.
+
 
 :end
 :notsupported
