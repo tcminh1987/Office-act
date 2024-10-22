5fc5111a0a6e971278f539d49849495158474cc2
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-10-22 09:28:00 +0700

    Update Project2021.cmd

--- Project2021.cmd
+++ Project2021.cmd
@@ -14,13 +14,6 @@ cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
 
 
 
-cscript //nologo ospp.vbs /inslic:"..\root\Licenses16\pkeyconfig-office.xrm-ms" >nul
-(for /f %%x in ('dir /b ..\root\Licenses16\client-issuance*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
-(for /f %%x in ('dir /b ..\root\Licenses16\projectprovl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
-(for /f %%x in ('dir /b ..\root\Licenses16\projectpro2021vl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
-
-
-
 cscript ospp.vbs /setprt:1688
 cscript ospp.vbs /inpkey:FTNWT-C6WBT-8HMGF-K9PRX-QV9H8
 cscript ospp.vbs /inpkey:J2JDC-NJCYY-9RGQ4-YXWMH-T3D4T
